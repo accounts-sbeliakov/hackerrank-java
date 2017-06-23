83fe98c89ee435e6cc4882e42ba7c13448b35a1f
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-06-24 00:15:17 +0100

    Create Solution.java

---
+++ introduction/java_stdin_stdout_ii/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int inInt = input.nextInt();
+    double inDouble = input.nextDouble();
+    input.nextLine();
+    String inString = input.nextLine();
+    System.out.println("String: " + inString);
+    System.out.println("Double: " + inDouble);
+    System.out.println("Int: " + inInt);
+  }
+}
