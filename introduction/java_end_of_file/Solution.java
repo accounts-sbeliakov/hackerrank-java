2ad4a1ac0ac9e6ea2b07a9bb95a161639e33d9e0
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-06-25 00:38:25 +0100

    Create Solution.java

---
+++ introduction/java_end_of_file/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  int stringNumber = 1;
+  while (input.hasNext()) {
+    String nextLine = new String("");
+    nextLine = nextLine + stringNumber + " ";
+    nextLine = nextLine + input.nextLine();
+    System.out.println(nextLine);
+    stringNumber++;
+  }
+  }
+}
