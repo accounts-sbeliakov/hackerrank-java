943db7549c5dac5b17a8cdf45a7cfe40b16c9c49
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-06-25 00:40:49 +0100

    Update Solution.java

--- introduction/java_end_of_file/Solution.java
+++ introduction/java_end_of_file/Solution.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Solution {
   public static void main(String[] args) {
-  Scanner input = new Scanner(System.in);
-  int stringNumber = 1;
-  while (input.hasNext()) {
-    String nextLine = new String("");
-    nextLine = nextLine + stringNumber + " ";
-    nextLine = nextLine + input.nextLine();
-    System.out.println(nextLine);
-    stringNumber++;
-  }
+    Scanner input = new Scanner(System.in);
+    int stringNumber = 1;
+    while (input.hasNext()) {
+      String nextLine = new String("");
+      nextLine = nextLine + stringNumber + " ";
+      nextLine = nextLine + input.nextLine();
+      System.out.println(nextLine);
+      stringNumber++;
+    }
   }
 }
