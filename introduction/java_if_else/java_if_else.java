495035531204dd4d1d0c7dd9f5cb928cd3c04aac
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-06-26 18:51:26 +0100

    Update java_if_else.java

--- introduction/java_if_else/java_if_else.java
+++ introduction/java_if_else/java_if_else.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 
 public class Solution {
+  private static String isWeird(int numberToCheck) {
+    String answer = new String();
+    if ((numberToCheck % 2 == 1) && (numberToCheck <= 100) && (numberToCheck >= 1)) {
+      answer = "Weird"; 
+    } else {
+        if((numberToCheck >= 2) && (numberToCheck <= 5)) { 
+          answer = "Not Weird"; 
+        }
+        if((numberToCheck >= 6) && (numberToCheck <= 20)) { 
+          answer = "Weird"; 
+        }
+        if((numberToCheck > 20) && (numberToCheck <= 100)) { 
+          answer = "Not Weird"; 
+        }
+      } 
+  return answer;
+  }
+  
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int inputNumber = input.nextInt();            
-    String output = "";
-    if ( (inputNumber % 2 == 1) && (inputNumber <= 100) && (inputNumber >= 1) ) { output = "Weird"; }
-    else {
-      if( (inputNumber >= 2) && (inputNumber <= 5) ) { output = "Not Weird"; }
-      if( (inputNumber >= 6) && (inputNumber <= 20) ){ output = "Weird"; }
-      if( (inputNumber > 20) && (inputNumber <= 100) ) { output = "Not Weird"; }
-    }
-    System.out.println(output);       
+    System.out.println(isWeird(inputNumber));       
   }
 }
