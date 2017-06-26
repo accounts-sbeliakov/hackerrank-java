7230899965861aee9fc9856469ff1fa4aeb4ef67
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-06-26 18:38:08 +0100

    Update Solution.java

--- introduction/java_static_initializer_block/Solution.java
+++ introduction/java_static_initializer_block/Solution.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Solution {
-  public static boolean flag = true; 
-  public static int breadth = 0;
-  public static int height = 0;
+  private static boolean flag = true; 
+  public static int breadth;
+  public static int height;
   static { 
     Scanner input = new Scanner(System.in); 
     breadth = input.nextInt();
     height = input.nextInt();
-    if ( (breadth <= 0) || (height <= 0) ) {
+    if ((breadth <= 0) || (height <= 0)) {
       System.out.println("java.lang.Exception: Breadth and height must be positive");
       flag = false;
    }
-}
+  }
 public static void main(String[] args){
-  if(flag){
+  if (flag) {
     int area = breadth * height;
 	  System.out.print(area);
 	}	
