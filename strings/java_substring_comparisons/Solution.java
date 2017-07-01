b8ee028fed8454bdacee756688f3387585303182
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-07-01 23:49:26 +0100

    Update Solution.java

--- strings/java_substring_comparisons/Solution.java
+++ strings/java_substring_comparisons/Solution.java
@@ -3,18 +3,26 @@ import java.lang.String;
 
 public class Solution {
   
-  public static String getSmallestAndLargest(String inputWord, int coefficient) {
-    String smallest = inputWord.substring(0, coefficient);
-    String largest = inputWord.substring(0, coefficient);
-    for (int i = 0; i <= inputWord.length() - coefficient; i++) {
-      if (inputWord.substring(i, i + coefficient).compareTo(smallest) < 0) {
-        smallest = inputWord.substring(i, i + coefficient);
+  private static String getSmallestSubstring(String inputWord, int length) {
+    String smallest = inputWord.substring(0, length);
+    for (int i = 0; i <= inputWord.length() - length; i++) {
+      String substring = inputWord.substring(i, i + length);
+      if (substring.compareTo(smallest) < 0) {
+        smallest = substring;
       } 
-      if (inputWord.substring(i, i + coefficient).compareTo(largest) > 0) {
-        largest = inputWord.substring(i, i + coefficient);
+    }       
+    return smallest;
+  }
+  
+  private static String getLargestSubstring(String inputWord, int length) {
+    String largest = inputWord.substring(0, length);
+    for (int i = 0; i <= inputWord.length() - length; i++) {
+      String substring = inputWord.substring(i, i + length);
+      if (substring.compareTo(largest) > 0) {
+        largest = substring;
       }
     }       
-    return smallest + "\n" + largest;
+    return largest;
   }
 
   public static void main(String[] args) {
@@ -22,6 +30,7 @@ public class Solution {
     String word = scan.next();
     int susbtringLength = scan.nextInt();
     scan.close();
-    System.out.println(getSmallestAndLargest(word, susbtringLength));
+    System.out.println(getSmallestSubstring(word, susbtringLength));
+     System.out.println(getLargestSubstring(word, susbtringLength));
   }
 }
