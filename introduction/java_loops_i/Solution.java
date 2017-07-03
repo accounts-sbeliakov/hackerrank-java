e61417b33b5cdc85acf259c9ed56a9c94975d220
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-07-03 07:37:59 +0100

    Update Solution.java

--- introduction/java_loops_i/Solution.java
+++ introduction/java_loops_i/Solution.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Solution {
   private static final int NUMBER_OF_ITERATIONS = 10;
-  public static void main(String[] args) {
+  public static void main (String[] args) {
     Scanner scan = new Scanner(System.in);
     int inputNumber = scan.nextInt();
-    for (int iterationNumber = 1; iterationNumber <= NUMBER_OF_ITERATIONS; iterationNumber++) {
-      int resultOfMultiplication;
-      resultOfMultiplication = inputNumber * iterationNumber;
-      System.out.printf("%d x %d = %d\n", inputNumber, iterationNumber, resultOfMultiplication);
+    for (int iteration = 1; iteration <= NUMBER_OF_ITERATIONS; iteration++) {
+      int resultOfMultiplication = inputNumber * iteration;
+      System.out.println(inputNumber + " x " + iteration + " = " + resultOfMultiplication);
     } 
   }
 }
