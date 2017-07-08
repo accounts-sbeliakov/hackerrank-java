a0d884d69e51053d660942fef16b8b862a377f6f
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-07-08 22:40:52 +0100

    Solve Java Date And Time problem (#14)
    * Create Solution.java
    
    * Update Solution.java

---
+++ introduction/java_date_and_time/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Solution {
+  
+  static private final String MONDAY = "MONDAY";
+  static private final String TUESDAY = "TUESDAY";
+  static private final String WEDNESDAY = "WEDNESDAY";
+  static private final String THURSDAY = "THURSDAY";
+  static private final String FRIDAY = "FRIDAY";
+  static private final String SATURDAY = "SATURDAY";
+  static private final String SUNDAY = "SUNDAY";
+  
+  private static String getDayOfWeekName(int dayOfWeekValue) {
+    switch (dayOfWeekValue) {
+      case Calendar.MONDAY: return MONDAY;
+      case Calendar.TUESDAY: return TUESDAY;
+      case Calendar.WEDNESDAY: return WEDNESDAY;
+      case Calendar.THURSDAY: return THURSDAY;
+      case Calendar.FRIDAY: return FRIDAY;
+      case Calendar.SATURDAY: return SATURDAY;
+      case Calendar.SUNDAY: return SUNDAY;
+    }
+    return "INVALID";
+  } 
+  
+  private static int getDayOfWeek(int year, int month, int day) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(year, month, day);
+    return calendar.get(Calendar.DAY_OF_WEEK);
+  }
+  
+  public static void main(String[] args) {
+    Scanner inputDate = new Scanner(System.in);
+    int month = inputDate.nextInt() - 1;
+    int day = inputDate.nextInt();
+    int year = inputDate.nextInt();
+    System.out.println(getDayOfWeekName(getDayOfWeek(year, month, day)));
+  }
+}
