fbb7f0bb278a4450311f77d002c1e586d016a296
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-07-09 22:40:05 +0100

    Update Solution.java

--- strings/java_substring/Solution.java
+++ strings/java_substring/Solution.java
@@ -1,17 +1,12 @@
-import java.util.Scanner;
 import java.lang.String;
+import java.util.Scanner;
 
 public class Solution {
-  
-  private static String findSubstring(String wordToCut, int startIndex, int endIndex) {
-    return wordToCut.substring(startIndex, endIndex);
-  }
-
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    String word = in.next();
+    String line = in.next();
     int start = in.nextInt();
     int end = in.nextInt();
-    System.out.println(findSubstring(word, start, end));
+    System.out.println(line.substring(start, end));
   }
 }
