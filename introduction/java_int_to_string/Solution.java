b05b20b819c2fbf9f4d3c492350c2d7f3dd8a931
Svetlana Beliakova <svetlana.nazarenko.18@gmail.com>
2017-07-09 22:54:27 +0100

    Update Solution.java

--- introduction/java_int_to_string/Solution.java
+++ introduction/java_int_to_string/Solution.java
@@ -22,19 +22,4 @@ public class Solution {
   }
 }
 
-public class DoNotTerminate {
-  public static class ExitTrappedException extends SecurityException {
-    private static final long serialVersionUID = 1;
-  }
-  public static void forbidExit() {
-    final SecurityManager securityManager = new SecurityManager() {
-      @Override
-      public void checkPermission(Permission permission) {
-        if (permission.getName().contains("exitVM")) {
-          throw new ExitTrappedException();
-        }
-      }
-    };
-    System.setSecurityManager(securityManager);
-  }
-}
+
